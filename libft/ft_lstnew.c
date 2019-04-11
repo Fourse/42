6779fe5717a667b0d2bcb2f0ce241d87edb42e34
Rupert Loraine <rloraine@mi-t5.21-school.ru>
2019-04-11 19:44:20 +0300

    u

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rloraine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/11 19:38:51 by rloraine          #+#    #+#             */
+/*   Updated: 2019/04/11 19:43:33 by rloraine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list newlink;
+
+	ne
+}
