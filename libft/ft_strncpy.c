fc4709fd15144c0e2e06d1fbbced4078e7db1a4b
Rupert Loraine <rloraine@mi-u3.21-school.ru>
2019-04-13 20:56:53 +0300

    u

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -6,11 +6,12 @@
 /*   By: rloraine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 12:41:51 by rloraine          #+#    #+#             */
-/*   Updated: 2019/04/05 15:34:42 by rloraine         ###   ########.fr       */
+/*   Updated: 2019/04/13 18:20:34 by rloraine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "libft.h"
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
