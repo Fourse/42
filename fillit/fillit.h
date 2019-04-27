e3f7ec5bbe195bb2a2ff7718d790b1e86534af7f
Rupert Loraine <rloraine@mi-t2.21-school.ru>
2019-04-27 18:07:18 +0300

    u

--- fillit/fillit.h
+++ fillit/fillit.h
@@ -1,39 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rloraine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/19 15:56:11 by rloraine          #+#    #+#             */
-/*   Updated: 2019/04/25 16:36:50 by rloraine         ###   ########.fr       */
+/*   Created: 2019/04/27 15:17:32 by rloraine          #+#    #+#             */
+/*   Updated: 2019/04/27 16:26:59 by rloraine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include "./libft/libft.h"
+#ifndef FILLIT_H
+# define FILLIT_H
 
-int	main(int argc, char **argv)
-{
-	int		fd;
-	char	*line;
-	int i;
-	i = 0;
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include "./libft/libft.h"
 
-
-	if (argc == 1)
-		fd = 0;
-	else if (argc == 2)
-		fd = open(argv[1], O_RDONLY);
-	else
-		return (2);
-	while (get_next_line(fd, &line) == 1)
-	{
-		ft_putendl(line);
-		free(line);
-		i++;
-	}
-	ft_putnbr(i);
-	if (argc == 2)
-		close(fd);
-}
+#endif
--- fillit/main.c
+++ fillit/main.c
@@ -5,16 +5,62 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rloraine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/22 19:23:58 by rloraine          #+#    #+#             */
-/*   Updated: 2019/04/22 19:25:11 by rloraine         ###   ########.fr       */
+/*   Created: 2019/04/27 15:24:59 by rloraine          #+#    #+#             */
+/*   Updated: 2019/04/27 18:07:10 by rloraine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fillit.h"
+
+void print_map(int fd, char **map)
+{
+	
+}
+
+int figure(int fd)
+{
+	int i;
+	int n;
+	int k;
+	static char buf[22];
+
+	i = 0;
+	n = 1;
+	read(fd, buf, 21);
+	ft_strsplit(buf, '\n');
+	while (buf[0][i] != '#')
+		i++;
+	k = i;
+	while (buf[n])
+	{
+		i = 0;
+		while (buf[n][i] != '#')
+		{
+			if (buf[n][i] == '#')
+				i == k ? n++, continue : return (0);
+			i++;
+		}
+		if (buf[n][i] == '#')
+			i == k ? n++, continue : return (0);
+	}
+	return (1);
+}
+
 int main(int argc, char **argv)
 {
+	char **map;
+
 	if (argc == 2)
 	{
-		
-	}
+		if (figure(open(argv[1], O_RDONLY)) == 0)
+		{
+			write(1, "error\n", 6);
+			return (0);
+		}
+		else if (figure(open(argv[1], O_RDONLY)) == 1)
+		{
+			print_square(fd, map);
+			return (0);
+		}
 	return (0);
 }
