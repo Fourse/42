af476c8e3c6ecc5664f8adb8c8b5dff3c145fac7
Rupert Loraine <rloraine@mi-t2.21-school.ru>
2019-05-28 16:25:16 +0300

    highway to hell

---
+++ ft_printf/libft/ft_strsub.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rloraine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/07 21:43:11 by rloraine          #+#    #+#             */
+/*   Updated: 2019/04/21 17:04:48 by rloraine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	if (s)
+	{
+		if (!(str = ft_strnew(len)))
+			return (NULL);
+		if (str == NULL)
+			return (NULL);
+		i = 0;
+		s = s + start;
+		while (i < len)
+		{
+			str[i] = s[i];
+			i++;
+		}
+		return (str);
+	}
+	return (NULL);
+}
