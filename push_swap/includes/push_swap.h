f47af5af8eea910d202a17da6bf8fd622e2bd119
Fourse <pozharsky.g@gmail.com>
2019-07-23 19:48:31 +0300

    ps beginning

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,13 +6,20 @@
 /*   By: rloraine <rloraine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 13:55:30 by rloraine          #+#    #+#             */
-/*   Updated: 2019/07/22 13:57:28 by rloraine         ###   ########.fr       */
+/*   Updated: 2019/07/23 16:09:03 by rloraine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "./ft_printf/includes/ft_printf.h"
+# include "../ft_printf/includes/ft_printf.h"
+
+typedef struct	s_stack
+{
+	int				ret;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}				t_stack;
 
 #endif
--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -1,19 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rloraine <rloraine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 13:55:20 by rloraine          #+#    #+#             */
-/*   Updated: 2019/07/22 14:10:20 by rloraine         ###   ########.fr       */
+/*   Updated: 2019/07/23 19:47:41 by rloraine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char **argv)
+void	stack_rev(t_stack **stack)
 {
-	
+	t_stack *prev;
+	t_stack *cur;
+	t_stack *next;
+
+	prev = NULL;
+	cur = *stack;
+	while (cur->next)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;		
+	}
+	*stack = prev;
+}
+
+int		main(int argc, char **argv)
+{
+	t_stack *stack_a;
+	int n;
+
+	n = 0;
+	while (argv[++n])
+	{
+		stack_a = (t_stack*)malloc(sizeof(t_stack));
+		stack_a->ret = ft_atoi(argv[n]);
+		stack_a = stack_a->next;
+		stack_a->next = NULL;
+	}
+	stack_rev(&stack_a);
 	return (0);
 }
