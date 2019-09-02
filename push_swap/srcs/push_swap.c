06b1f8146c401b12bcf98af643282c94c3a8369e
Fourse <pozharsky.g@gmail.com>
2019-09-02 17:35:03 +0300

    sorting 100 numbers done

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,28 +6,13 @@
 /*   By: rloraine <rloraine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 13:55:20 by rloraine          #+#    #+#             */
-/*   Updated: 2019/09/01 18:51:13 by rloraine         ###   ########.fr       */
+/*   Updated: 2019/09/02 17:30:32 by rloraine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 int g_i = 0;
-
-void	free_stack(t_stack **a, int size)
-{
-	t_stack *tmp;
-
-	while (size)
-	{
-		tmp = (*a)->next;
-		free((*a)->prev);
-		free((*a)->next);
-		free((*a));
-		(*a) = tmp;
-		--size;
-	}
-}
 
 void	sort(t_stack **a, int size, long *comm)
 {
@@ -40,7 +25,7 @@ void	sort(t_stack **a, int size, long *comm)
 		sort_3(a, size, comm);
 	else if (size <= 5)
 		sort_5(a, &b, size, comm);
-	else if (size <= 100)
+	else if (size <= 500)
 		sort_100(a, &b, size, comm);
 }
 
@@ -59,8 +44,6 @@ int		main(int argc, char **argv)
 	{
 		sort(&a, size, &comm);
 	}
-	free_stack(&a, size);
-	free(arr);
 	//printf("%d\n", g_i);
 	return (0);
 }
--- push_swap/srcs/sorting_100.c
+++ push_swap/srcs/sorting_100.c
@@ -6,7 +6,7 @@
 /*   By: rloraine <rloraine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/24 14:27:27 by rloraine          #+#    #+#             */
-/*   Updated: 2019/09/01 18:32:38 by rloraine         ###   ########.fr       */
+/*   Updated: 2019/09/02 17:29:30 by rloraine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 int		share_stack(t_stack **a, t_stack **b, int size, long *comm)
 {
-	int next;
-	int prev;
 	int mid;
 	int prev_mid;
 	int check_mid;
 	int count;
 
 	mid = find_mid(*a, size);
-	prev_mid = 0;
+	prev_mid = find_min(*a, size);
 	check_mid = mid;
 	count = 0;
 	while (1)
@@ -43,12 +41,7 @@ int		share_stack(t_stack **a, t_stack **b, int size, long *comm)
 			--size;
 		}
 		else
-		{
-			next = 0;
-			prev = 0;
-			find_dir(*a, mid, &next, &prev);
-			next >= prev ? rev_rotate(a, comm, 1) : rotate(a, comm, 1);
-		}
+			rotate(a, comm , 1);
 	}
 	return (count);
 }
@@ -94,8 +87,6 @@ void	get_up(t_stack **a, long *ops)
 void	sort_100(t_stack **a, t_stack **b, int size, long *comm)
 {
 	int count;
-	int next;
-	int prev;
 
 	if (size == 2 && (*a)->num > (*a)->next->num)
 		swap(a, comm, 1);
